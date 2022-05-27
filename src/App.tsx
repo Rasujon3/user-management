6317235326e296c9fa80261dafb554e03c6ab9c1
Rasujon3 <rasujon3@gmail.com>
2022-05-27 21:10:16 +0600

    78_5-3 Function parameter types, return types, union type, any

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,26 @@ let fees: number[] = [12, 45, 78, 12, 45];
 interface Person {
   name: string;
   job?: string;
+  employed: string | boolean;
   age: number;
+  location?: string | number;
 }
 
 const person: Person = {
   name: "Bill Clinton",
-
+  employed: true,
   age: 85,
+  location: 55,
+};
+
+const handleAddUser = (
+  firstName: string,
+  age: number,
+  address: string
+): number => {
+  const total: number = 50;
+  console.log(firstName, age, address);
+  return age;
 };
 
 function App() {
