a3f6b88501083dc5b170c2b622166727157b6c5d
Rasujon3 <rasujon3@gmail.com>
2022-05-27 23:47:17 +0600

    78_5-6 data type model and load date and display them

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,18 +1,24 @@
 import React, { FC } from "react";
+import UserModel from "./../types/models";
+
+// interface Props {
+//   name: string;
+//   age: number;
+//   addUser: () => void;
+// }
 
 interface Props {
-  name: string;
-  age: number;
-  addUser: () => void;
+  user: UserModel;
+  addUser: (user: UserModel) => void;
 }
 
-const User: FC<Props> = ({ name, age, addUser }) => {
+const User: FC<Props> = ({ user, addUser }) => {
   return (
     <div>
       <h2>
-        Hello from : {name}, with experience {age}
+        Hello from : {user.name}, with experience {user.email}
       </h2>
-      <button onClick={addUser}>Add Me</button>
+      <button onClick={() => addUser(user)}>Add Me</button>
     </div>
   );
 };
--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import User from "./User";
+import UserModel from "./../types/models";
 
 const Users = () => {
-  const handleAdduser = (): void => {};
+  const [users, setUsers] = useState<UserModel[]>([]);
+  const [team, setTeam] = useState<UserModel[]>([]);
+
+  useEffect(() => {
+    const url = `https://jsonplaceholder.typicode.com/users`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const handleAdduser = (user: UserModel): void => {
+    const newTeam = [...team, user];
+    setTeam(newTeam);
+  };
   return (
     <div>
-      <h2>My Users:</h2>
-      <User name="Rohomot" age={55} addUser={handleAdduser} />
+      <h2>My Users: {users.length}</h2>
+      <h3>Team size: {team.length}</h3>
+      {/* <User name="Rohomot" age={55} addUser={handleAdduser} /> */}
+      {users.map((user) => (
+        <User user={user} addUser={handleAdduser} />
+      ))}
     </div>
   );
 };
